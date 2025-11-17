25c6f19bbb181a8859f357f04c25505646efce62
unknown <zuzanna.s8553@gmail.com>
2025-11-17 13:04:31 +0100

    zadania

---
+++ zad_1.py
@@ -0,0 +1,3 @@
+def imie(name: str, surname: str) -> str:
+    return(f'Cześć {name} {surname} ')
+print(imie('Jan', 'Kowalski'))
---
+++ zad_2.py
@@ -0,0 +1,3 @@
+def mnozenie(a: int, b: int) -> int:
+    return a*b
+print(mnozenie(2,4))
